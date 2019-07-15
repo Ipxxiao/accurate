fb3485a99b4bc7ed77f34d73b8521fbc664f12e4
Ipxxiao <jonashsiao@icloud.com>
2019-07-15 17:48:21 +0800

    add balel

--- src/accurate.js
+++ src/accurate.js
@@ -1,123 +1,126 @@
+import 'babel-core/register';
+import 'babel-polyfill';
+
 function isNum(params) {
-    var flag = true;
+	var flag = true;
 
-    for (var i in params) {
-        try {
-            if (isNaN(params[i]) || typeof params[i] != 'number') {
-                flag = false;
-                throw new TypeError(`The ${++i}th parameter type is not a number`);
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    }
+	for (var i in params) {
+		try {
+			if (isNaN(params[i]) || typeof params[i] != 'number') {
+				flag = false;
+				throw new TypeError(`The ${++i}th parameter type is not a number`);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    return flag;
+	return flag;
 }
 
 function getDecimalDigits(num) {
-    try {
-        return num.toString().split('.')[1].length;
-    } catch (e) {
-        return 0;
-    }
+	try {
+		return num.toString().split('.')[1].length;
+	} catch (e) {
+		return 0;
+	}
 }
 
 module.exports = {
-    // 精度加法计算
-    add(num1, num2) {
-        if (!isNum(arguments)) {
-            return NaN;
-        }
+	// 精度加法计算
+	add(num1, num2) {
+		if (!isNum(arguments)) {
+			return NaN;
+		}
 
-        var me = this;
-        var temp = 0,
-            l1 = 0,
-            l2 = 0,
-            m = 0;
+		var me = this;
+		var temp = 0,
+			l1 = 0,
+			l2 = 0,
+			m = 0;
 
-        l1 = getDecimalDigits(num1);
-        l2 = getDecimalDigits(num2);
+		l1 = getDecimalDigits(num1);
+		l2 = getDecimalDigits(num2);
 
-        m = 10 ** Math.max(l1, l2);
-        temp = me.mul(num1, m) + me.mul(num2, m);
+		m = 10 ** Math.max(l1, l2);
+		temp = me.mul(num1, m) + me.mul(num2, m);
 
-        return temp / m;
-    },
-    // 精度减法计算
-    subtract(num1, num2) {
-        if (!isNum(arguments)) {
-            return NaN;
-        }
+		return temp / m;
+	},
+	// 精度减法计算
+	subtract(num1, num2) {
+		if (!isNum(arguments)) {
+			return NaN;
+		}
 
-        var me = this;
+		var me = this;
 
-        return me.add(num1, -num2);
-    },
-    // 精度乘法计算
-    mul(num1, num2) {
-        if (!isNum(arguments)) {
-            return NaN;
-        }
+		return me.add(num1, -num2);
+	},
+	// 精度乘法计算
+	mul(num1, num2) {
+		if (!isNum(arguments)) {
+			return NaN;
+		}
 
-        var me = this;
-        var temp = 0,
-            l1 = 0,
-            l2 = 0,
-            m = 0,
-            s1 = num1.toString(),
-            s2 = num2.toString();
+		var me = this;
+		var temp = 0,
+			l1 = 0,
+			l2 = 0,
+			m = 0,
+			s1 = num1.toString(),
+			s2 = num2.toString();
 
-        l1 = getDecimalDigits(num1);
-        l2 = getDecimalDigits(num2);
+		l1 = getDecimalDigits(num1);
+		l2 = getDecimalDigits(num2);
 
-        m = 10 ** (l1 + l2);
-        temp = Number(s1.replace('.', '')) * Number(s2.replace('.', ''));
+		m = 10 ** (l1 + l2);
+		temp = Number(s1.replace('.', '')) * Number(s2.replace('.', ''));
 
-        return temp / m;
-    },
-    // 精度除法计算
-    division(num1, num2) {
-        if (!isNum(arguments)) {
-            return NaN;
-        }
+		return temp / m;
+	},
+	// 精度除法计算
+	division(num1, num2) {
+		if (!isNum(arguments)) {
+			return NaN;
+		}
 
-        var me = this;
-        var temp = 0,
-            l1 = 0,
-            l2 = 0,
-            m = 0,
-            s1 = num1.toString(),
-            s2 = num2.toString();
+		var me = this;
+		var temp = 0,
+			l1 = 0,
+			l2 = 0,
+			m = 0,
+			s1 = num1.toString(),
+			s2 = num2.toString();
 
-        l1 = getDecimalDigits(num1);
-        l2 = getDecimalDigits(num2);
+		l1 = getDecimalDigits(num1);
+		l2 = getDecimalDigits(num2);
 
-        m = 10 ** (l2 - l1);
-        temp = Number(s1.replace('.', '')) / Number(s2.replace('.', ''));
+		m = 10 ** (l2 - l1);
+		temp = Number(s1.replace('.', '')) / Number(s2.replace('.', ''));
 
-        return me.mul(temp, m);
-    },
-    // 精度取模计算
-    modulo(num1, num2) {
-        if (!isNum(arguments)) {
-            return NaN;
-        }
+		return me.mul(temp, m);
+	},
+	// 精度取模计算
+	modulo(num1, num2) {
+		if (!isNum(arguments)) {
+			return NaN;
+		}
 
-        var me = this;
-        var temp = 0,
-            l1 = 0,
-            l2 = 0,
-            m = 0;
+		var me = this;
+		var temp = 0,
+			l1 = 0,
+			l2 = 0,
+			m = 0;
 
-        l1 = getDecimalDigits(num1);
-        l2 = getDecimalDigits(num2);
+		l1 = getDecimalDigits(num1);
+		l2 = getDecimalDigits(num2);
 
-        m = 10 ** Math.max(l1, l2);
-        num1 = me.mul(num1, m);
-        num2 = me.mul(num2, m);
-        temp = num1 % num2;
+		m = 10 ** Math.max(l1, l2);
+		num1 = me.mul(num1, m);
+		num2 = me.mul(num2, m);
+		temp = num1 % num2;
 
-        return temp / m;
-    },
+		return temp / m;
+	},
 };
